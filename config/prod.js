08c20e979c2fc950f2e48136adf2fc0deec2de48
Ryan Dougan <dougieled@hotmail.co.uk>
2022-05-02 13:47:36 +0800

    test using env variable for port

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 // prod.js - production keys here
 module.exports = {
   host: process.env.HOST,
-  port: 3306,//process.env.PORT,
+  port: process.env.MYPORT,
   user: process.env.DBUSER,
   password: process.env.DBPASSWORD,
   database: process.env.DATABASE,
